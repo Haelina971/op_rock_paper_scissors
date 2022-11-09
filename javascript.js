1bdd9844cabc50163c46d5f03d9123312f4be8b4
Haelina <tarany31@gmail.com>
2022-11-08 21:58:33 -0600

    Added UI in HTML: - title - 3 input used as buttons for choices - 1 div to display points
    In CSS:
    - Added fonts
    - Started alignment
    
    In JS:
    - Event listeners on input (buttons)
    - endOfGame() function changing body content when player or computer
    arrives at 5 points
    - reset() function creating an input (button) to reload a new game

--- javascript.js
+++ javascript.js
@@ -1,32 +1,75 @@
-const choices = ["Rock", "Paper", "Scissors"];
+/* Array containing the three choices */
+const choices = ["Squirtle", "Bulbasaur", "Charmander"];
 let playerSelection;
 let computerSelection;
 let userVictory = 0;
 let computerVictory = 0;
+let gameOn = true; //delete?
+const body = document.querySelector('body');
 
+/* Get random choice for computer */
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+/* Play one round and determine the winner */
 function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);   //Capitalize result
+    const results = document.querySelector('#results'); //Get div for displaying results
+    const score = document.querySelector('#score');
     if(playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if(playerSelection === "Rock" && computerSelection === "Scissors") {
+        results.textContent = "It's a tie!";
+    } else if(
+        (playerSelection === "Squirtle" && computerSelection === "Charmander") ||
+        (playerSelection === "Bulbasaur" && computerSelection === "Squirtle")||
+        (playerSelection === "Charmander" && computerSelection === "Bulbasaur")) {
         userVictory++;
-        return "You won! Rock beats Scissors!";
-    } else if(playerSelection === "Paper" && computerSelection === "Rock") {
-        userVictory++;
-        return "You won! Paper beats Rock!";
-    } else if(playerSelection === "Scissors" && computerSelection === "Paper") {
-        userVictory++;
-        return "You won! Scissors beats Paper!";
+        results.textContent = `You won! ${playerSelection} beats ${computerSelection}!`;
     } else {
         computerVictory++;
-        return `You loose! ${computerSelection} beats ${playerSelection}!`;
+        results.textContent = `You loose! ${computerSelection} beats ${playerSelection}!`;
+    }
+    score.textContent = `${userVictory} : ${computerVictory}`;
+}
+
+/* Add event listeners to buttons */
+const buttons = document.querySelectorAll('input');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+    playRound(button.id, getComputerChoice());
+    if(userVictory == 5 || computerVictory == 5) {
+        endOfGame();
+    }
+    });
+});
+
+function endOfGame() {
+    if(userVictory == 5) {
+        body.textContent = `You won! You have ${userVictory} points against ${computerVictory}!`;
+    } else {
+        body.textContent = `You lost! You have ${userVictory} points against ${computerVictory}!`;
+    reset();
     }
 }
 
-function game() {
+function reset() {
+    const resetButton = document.createElement('input');
+    resetButton.setAttribute('type', 'image');
+    resetButton.setAttribute('id', 'reset');
+    resetButton.setAttribute('src', 'images/pokeball.png');
+    body.appendChild(resetButton);
+    resetButton.addEventListener('click', () => document.location.reload());
+}
+
+
+
+
+
+
+
+
+/* First part of the project (console-based) */
+/*function game() {
     for(let i=0; i < 5; i++) {
         playerSelection = window.prompt("Rock? Paper? Scissors?");
         playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
@@ -44,6 +87,7 @@ function game() {
     } else {
         console.log(`You lost! You have ${userVictory} points against ${computerVictory}!`)
     }
-}
+} 
 
-game();
+game();*/
+
