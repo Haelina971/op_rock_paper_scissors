c7465a9472269664e00176e142fd9af89183928a
Haelina <tarany31@gmail.com>
2022-11-15 22:03:08 -0600

    In HTML: - Delete the score div
    In CSS:
    - Added style for the final result page
    
    In Javascript:
    - Refactor of code by simplifying and generalizing displayElement()
    displayPoints()
    - Put eventlisteners and playRound() call inside of game()
    - Modification reset() and endOfGame() to style with CSS

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
-let playerSelection;
-let computerSelection;
 let userVictory = 0;
 let computerVictory = 0;
-let winner;
-let gameOn = true; //delete?
+const results = document.querySelector('.results');
+let imgPlayerElement = document.querySelector('.user_element');
+let imgCompElement = document.querySelector('.comp_element');
+const userPoints = document.querySelectorAll('div.player_points > img');
+const compPoints = document.querySelectorAll('div.computer_points > img');
 const body = document.querySelector('body');
 
 /* Get random choice for computer from an array containing the different choices */
@@ -15,9 +16,8 @@ function getComputerChoice() {
 /* Play one round and determine the winner */
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);   //Capitalize result
-    const results = document.querySelector('.results'); //Get div for displaying results
-    const score = document.querySelector('.score');
-    displayElement(playerSelection, computerSelection);
+    displayElement(playerSelection, imgPlayerElement);
+    displayElement(computerSelection, imgCompElement);
     if(playerSelection === computerSelection) {
         results.textContent = "It's a tie!";
     } else if(
@@ -30,75 +30,73 @@ function playRound(playerSelection, computerSelection) {
         computerVictory++;
         results.textContent = `You loose! ${computerSelection} beats ${playerSelection}!`;
     }
-    score.textContent = `${userVictory} : ${computerVictory}`;
-    displayUserPoints();
-    displayCompPoints();
+    displayPoints(userVictory, userPoints);
+    displayPoints(computerVictory, compPoints);
 }
 
-/* Add event listeners to buttons */
-const buttons = document.querySelectorAll('input');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-    playRound(button.id, getComputerChoice());
-    if(userVictory == 5 || computerVictory == 5) {
-        endOfGame();
-    }
+/* Play rounds until user or computer reaches 5 points */
+function game() {
+    /* Add event listeners to buttons */
+    const buttons = document.querySelectorAll('input');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+        playRound(button.id, getComputerChoice());
+        if(userVictory == 5 || computerVictory == 5) {
+            endOfGame();
+        }
+        });
     });
-});
+}
 
+
+/* Display final results and winner + call reset() to restart game */
 function endOfGame() {
     if(userVictory == 5) {
         body.textContent = `You won! You have ${userVictory} points against ${computerVictory}!`;
     } else {
         body.textContent = `You lost! You have ${userVictory} points against ${computerVictory}!`;
     }
+    body.className = 'body_final';
     reset();
 }
 
+/* Display button to play a new game */
 function reset() {
+    const playAgain = document.createElement('div');
+    playAgain.className = 'play_again';
+    playAgain.textContent = "To play again, click the Pokeball";
     const resetButton = document.createElement('input');
     resetButton.setAttribute('type', 'image');
-    resetButton.setAttribute('id', 'reset');
     resetButton.setAttribute('src', 'images/pokeball.png');
+    body.appendChild(playAgain);
     body.appendChild(resetButton);
     resetButton.addEventListener('click', () => document.location.reload());
 }
 
-function displayElement(playerSelection, computerSelection) {
-    let imgPlayerElement = document.querySelector('.user_element');
-    let imgCompElement = document.querySelector('.comp_element');
-    if(playerSelection == "Squirtle") {
-        imgPlayerElement.setAttribute('src', 'images/water.png');
-    } else if(playerSelection == 'Charmander') {
-        imgPlayerElement.setAttribute('src', 'images/fire.png');
+/* Display element images related to choice of computer and player 
+choice = choice of player or comp
+user = HTML element of player of comp element */
+function displayElement(choice, user) {
+    if(choice == "Squirtle") {
+        user.setAttribute('src', 'images/water.png');
+    } else if(choice == "Charmander") {
+        user.setAttribute('src', 'images/fire.png');
     } else {
-        imgPlayerElement.setAttribute('src', 'images/leaf.png');
-    }
-    if(computerSelection == 'Squirtle') {
-        imgCompElement.setAttribute('src', 'images/water.png');
-    } else if(computerSelection == 'Charmander') {
-        imgCompElement.setAttribute('src', 'images/fire.png');
-    } else {
-        imgCompElement.setAttribute('src', 'images/leaf.png');
+        user.setAttribute('src', 'images/leaf.png');
     }
 }
 
-function displayUserPoints() {
-    const userPoints = document.querySelectorAll("div.player_points > img");
-    for(let i = 0; i < userVictory; i++) {
-        userPoints[i].style.opacity = '100%';
+/* Lightup Pokeballs to track points 
+victory = tracking of number of points of player or comp
+user = HTML element of player or comp pokeballs*/
+function displayPoints(victory, user) {
+    for(let i=0; i<victory; i++) {
+        user[i].style.opacity = '100%';
     }
 }
 
-function displayCompPoints() {
-    const compPoints = document.querySelectorAll("div.computer_points > img");
-    for(let i = 0; i < computerVictory; i++) {
-        compPoints[i].style.opacity = '100%';
-    }
-}
-
-
-
+/* Play the gane */
+game();
 
 
 
